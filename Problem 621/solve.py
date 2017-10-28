e6bc4a160b127a6fe727e5b27615175af793db13
bypie5 <bypie966@gmail.com>
2017-10-28 11:46:29 -0700

    Test case works

--- Problem 621/solve.py
+++ Problem 621/solve.py
@@ -6,7 +6,19 @@ for i in range(0,lines):
 	curr_result = input()
 	encypted_results.append(str(curr_result))
 
-# positive result
 for result in encypted_results:
+	# positive result
 	if result == "1" or result == "4" or result == "78":
 		print("+") 
+
+	# negative result
+	if result.endswith("35"):
+		print("-")
+
+	# Failed
+	if result.startswith("9") and result.endswith("4"):
+ 		print ("*")
+
+ 	# Not Completed
+ 	if result.startswith("190"):
+ 		print("?")

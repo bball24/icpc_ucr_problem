2802386f17bbc4691c584234fd84a8f32ed308f6
bypie5 <bypie966@gmail.com>
2017-10-28 12:25:33 -0700

    Make deal class

---
+++ 555/solve.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Deal () {
+public:
+	Deal(string dealer, string cards_1, string cards_2);
+}
+
+
+
+int main () {
+	string dealer;
+	getline(cin, dealer);
+
+	string cards_1;
+	getline(cin, cards_1);
+
+	string cards_2;
+	getline(cin, cards_2);
+}

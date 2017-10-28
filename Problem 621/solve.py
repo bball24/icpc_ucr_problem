5c4ae37f17cc334c0b2621a629c531ad0b3dafcd
bypie5 <bypie966@gmail.com>
2017-10-28 11:32:05 -0700

    Make test for positive result

--- Problem 621/solve.py
+++ Problem 621/solve.py
@@ -4,9 +4,9 @@ encypted_results = []
 
 for i in range(0,lines):
 	curr_result = input()
-	encypted_results.append(curr_result)
+	encypted_results.append(str(curr_result))
 
 # positive result
 for result in encypted_results:
-	if result == 1 or result == 4 or result == 78:
+	if result == "1" or result == "4" or result == "78":
 		print("+")

ab34a41c99bdb52a5d4f44d17d45c5c1a8b63e71
bypie5 <bypie966@gmail.com>
2017-10-28 11:29:28 -0700

    Init commit

---
+++ Problem 621/solve.py
@@ -0,0 +1,12 @@
+lines = input()
+
+encypted_results = []
+
+for i in range(0,lines):
+	curr_result = input()
+	encypted_results.append(curr_result)
+
+# positive result
+for result in encypted_results:
+	if result == 1 or result == 4 or result == 78:
+		print("+")
